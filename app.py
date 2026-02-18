9fefff2285778067f261a5b33eb5ab44e27ca004
Snehajha27 <sneha.jha2021@gmail.com>
2026-02-18 22:01:16 +0530

    Remove unused CountVectorizer import
    Removed unused import for CountVectorizer.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import re
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix
 
